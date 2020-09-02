3383a1c547395c20819dd88264e4f90cfca842d9
chenguanghui <gchen3544@gmail.com>
2020-09-02 21:20:20 +0800

    -

--- system/base.go
+++ system/base.go
@@ -1,12 +1,15 @@
 package system
 
+import "errors"
+
 const (
 	Name    = "GoBlog"               // 系统名称
-	Version = "1.0.0"                // 系统版本
+	Version = ".gitignore.0.0"       // 系统版本
 	HomeURL = "http://www.04559.com" // 系统官方网址
 	Author  = "ChenGuangHui"         // 系统作者
 )
 
+// ConfMySQL MySQL配置参数
 type ConfMySQL struct {
 	Enable   bool   `json:"enable"`
 	Address  string `json:"address"`
@@ -18,12 +21,16 @@ type ConfMySQL struct {
 	MaxOpen  uint16 `json:"max_open"`
 }
 
+// ConnectMySQL 连接MySQL 成功返回nil
 func ConnectMySQL(c *ConfMySQL) error {
 	if !c.Enable {
 		return nil
 	}
 	if c.Dbname == "" {
 		c.Dbname = c.Username
+	}
+	if c.Address == "" || c.Username == "" {
+		return errors.New("MySQL conf incomplete")
 	}
 
 	return nil
